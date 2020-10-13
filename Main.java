cb1b6c690115ebc15848a68bfbec3a3e84839b20
aydig8139 <aydig8139@wrdsb.ca>
2020-10-13 20:25:07 +0000

    unfinished question

--- Main.java
+++ Main.java
@@ -1,15 +1,41 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ * returns whether every digit is positive or negative
+ * @author Rose
  */
 public class Main {
+
+  public static void allDigitsOdd(int integer){
+
+    
+    int div = 0;
+
+    while(integer >= 1){
+      div = integer % 2;
+      integer = integer/10;
+      
+    }
+    
+    if (div == 0){
+      System.out.println("false");
+    }else {
+      System.out.println("true");
+    }
+
+  }
 
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create new Scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("enter integer");
+    int integer = input.nextInt();
+    allDigitsOdd(integer);
     
   }
 }
