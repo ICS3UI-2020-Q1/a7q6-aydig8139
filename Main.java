4a60e5f31dde3ded77a168d1f62095f750519d53
aydig8139 <aydig8139@wrdsb.ca>
2020-10-14 13:55:27 +0000

    finished true or false question

--- Main.java
+++ Main.java
@@ -6,22 +6,24 @@ import java.util.Scanner;
  */
 public class Main {
 
-  public static void allDigitsOdd(int integer){
+  //divides the integer and stores the remainder
+  public static boolean allDigitsOdd(int integer){
 
-    
+    //declare a variable that will store the remainder value
     int div = 0;
 
+    //create a while loop that will keep dividing the integer to check each of the digits if they are odd or even
     while(integer >= 1){
       div = integer % 2;
       integer = integer/10;
-      
+
+      //write an if statement that returns false if the remainder is 0 which means the digit is even
+      if (div == 0){
+        return false;
+      }
     }
-    
-    if (div == 0){
-      System.out.println("false");
-    }else {
-      System.out.println("true");
-    }
+    //if the above if statement is not true for any of the numbers, return true
+    return true;
 
   }
 
@@ -33,9 +35,12 @@ public class Main {
     // create new Scanner for user input
     Scanner input = new Scanner(System.in);
 
-    System.out.println("enter integer");
+    //ask user for an integer and store it
+    System.out.println("Please enter an integer");
     int integer = input.nextInt();
-    allDigitsOdd(integer);
     
+    //declare a boolean that is equal to the method and print the boolean result on the screen
+    boolean valid = allDigitsOdd(integer);
+    System.out.println(valid);
   }
 }
